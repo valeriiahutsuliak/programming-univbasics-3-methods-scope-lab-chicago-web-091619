4a2e5fb4d0780369019dec01983910ddd6f3e95f
Valerria Hutsuliak <valeria.gutsuliak@gmail.com>
2019-10-03 18:03:09 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -9,3 +9,9 @@ end
 def catch_phrase(phrase)
   puts phrase	  
 end	
+describe "#catch_phrase" do 
+  it "puts out a catch phrase" do 	   
+    phrase = "It's-a me, Mario!"	    
+    expect{catch_phrase}.to output("It's-a me, Mario!\n").to_stdout
+  end	  
+  end
